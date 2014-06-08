56ce8fe3425cc486a35365e2b95c971e1a07da41
Ealden Esto E. Escañan <ealden@escanan.com>
2014-06-08 21:51:07 +0800

    Was missing a new line

--- RomanNumeralsKataTest/RomanNumeralConverterTest.cs
+++ RomanNumeralsKataTest/RomanNumeralConverterTest.cs
@@ -14,6 +14,7 @@ namespace RomanNumeralsKataTest
         {
             converter = new RomanNumeralConverter();
         }
+
         [TestMethod]
         public void ShouldReturn1ForI()
         {
